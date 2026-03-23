966113b2eea626b48af3825a6248f96ed30e43df
Francisco Lopez Tancredi <flopezt@fi.uba.ar>
2026-03-23 17:22:20 -0300

    feat: add validar-echo-server.sh to validate echo server

---
+++ validar-echo-server.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+MESSAGE="hello world"
+RESPONSE=$(docker run --rm --network tp0_testing_net alpine:latest sh -c "echo '$MESSAGE' | nc -w 5 server 12345")
+
+if [ "$RESPONSE" == "$MESSAGE" ]; then
+    echo "action: test_echo_server | result: success"
+else
+    echo "action: test_echo_server | result: fail"
+    exit 1
+fi
