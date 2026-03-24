992ab945d1a575adc17e9b4da7a8892133b428c7
Francisco Lopez Tancredi <flopezt@fi.uba.ar>
2026-03-24 12:50:51 -0300

    fix(validar-echo-server): use = instead of ==

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -3,7 +3,7 @@
 MESSAGE="hello world"
 RESPONSE=$(docker run --rm --network tp0_testing_net alpine:latest sh -c "echo '$MESSAGE' | nc -w 5 server 12345")
 
-if [ "$RESPONSE" == "$MESSAGE" ]; then
+if [ "$RESPONSE" = "$MESSAGE" ]; then
     echo "action: test_echo_server | result: success"
 else
     echo "action: test_echo_server | result: fail"
